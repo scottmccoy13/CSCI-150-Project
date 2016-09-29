de7f3933b6d34a0e299e3de0462853ce8f8cb445
Mai Xiong <xiongdmai@mail.fresnostate.edu>
2016-09-29 14:48:32 -0700

    Add Customer Search and Details
    For now Customer Search only compare the search against the first_name
    (not case sensitive).
    I also moved the templates that were not in templates/CustomerData/ into
    it.

--- CustomerData/models.py
+++ CustomerData/models.py
@@ -7,13 +7,6 @@ class Customer(models.Model):
 	first_name = models.CharField(max_length = 250)
 	last_name = models.CharField(max_length = 250)
 	
-	#> python manage.py shell
-	#> from CustomerData.models import Customer
-	#> Customer.objects.all()
-			#for an example test, I added a Customer with first_name = Customer_fname and last_name = Customer_lname
-	#output should be: 
-	#<QuerySet [<Customer: Customer_fname Customer_lname>]>
-	
 	def get_absolute_url(self):
 		return reverse('CustomerData:index')
 		
--- CustomerData/views.py
+++ CustomerData/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django import forms
 from .models import Customer
 
 def index(request):
-	return render(request, 'index.html')
+	return render(request, 'CustomerData/index.html')
 	
 class CustomerCreate(CreateView):
-		model = Customer
-		fields = ['first_name', 'last_name']
+	model = Customer
+	fields = ['first_name', 'last_name']
+
+class DetailView(generic.DetailView):
+	model = Customer
+	template_name = 'CustomerData/detail.html'
 		
 class CustomerList(generic.ListView):
 		template_name = 'customer_list.html'
@@ -21,13 +26,27 @@ class CustomerDelete(DeleteView):
 		model = Customer
 		success_url = reverse_lazy('CustomerData:customer_list')
 
+class CustomerSearchForm(forms.Form):
+	search = forms.CharField(max_length = 500)
+
 def CustomerSearch(request):
-	if request.method == "post":
-		search = request.POST["search"]
-		try:
-			#
-			return #
-		except Customer.DoesNotExist:
-			return #
+	search_list = Customer.objects.all()
+	context = {
+				"search_list" : search_list
+	}
+	if request.method == 'POST':
+		form = CustomerSearchForm(request.POST)
+		if form.is_valid():
+			search = form.cleaned_data['search']
+			search_list = Customer.objects.filter(first_name__icontains = search)	
+			context = {
+				"search_list" : search_list
+			}
+			return render(request,'CustomerData/search_results.html', context)
 	else:
-		return render(request, 'search_for_customer.html')
+		form = CustomerSearchForm()
+	
+	return render(request, 'CustomerData/search_for_customer.html', {'form':form}, context)
+
+def CustomerResults(request):
+	return render(request, 'search_results.html')
