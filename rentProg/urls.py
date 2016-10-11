793d827679525ee3f3ea945d618c27e68b09ced3
scottmccoy13 <scottmccoy13@gmail.com>
2016-10-11 16:39:41 -0700

    Update urls.py

--- rentProg/urls.py
+++ rentProg/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-	url(r'^CustomerData/', include('CustomerData.urls')),
+    url(r'^admin/', admin.site.urls)
+    url(r'^CustomerData/', include('CustomerData.urls')),
+    url(r'^calendar/', include('Calendar.urls')),
 ]
