cd6645e75f105ae38c482f90442fb04527e4f5eb
scottmccoy13 <scottmccoy13@gmail.com>
2016-10-18 16:40:38 -0700

    Asset data commit 18 Oct.

--- AssetData/urls.py
+++ AssetData/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from AssetData import views
 
 urlpatterns = [
-    url(r'^$', views.dummy)
+    url(r'^$', views.assetHome), # /assetdata/
+    url(r'^(?P<asset_id>[0-9]+)/$', views.assetDetail, name='assetDetail'), #
 ]
--- AssetData/views.py
+++ AssetData/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from .models import Asset
 
-
-def dummy(request):
-    return HttpResponse()
+def assetHome(request):
+    return HttpResponse("<h1>Asset Data</h1>") 
+	
+def assetDetail(request, asset_id):
+	return HttpResponse("<h2>Details for asset ID:" + str(asset_id) + "</h2>")
