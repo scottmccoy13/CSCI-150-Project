109ba9e253eb47a18a7806f743001076a96313f3
scottmccoy13 <scottmccoy13@gmail.com>
2016-10-25 16:55:58 -0700

    Commit 25 Oct 2016

--- AssetData/urls.py
+++ AssetData/urls.py
@@ -5,4 +5,5 @@ from AssetData import views
 urlpatterns = [
     url(r'^$', views.assetHome), # /assetdata/
     url(r'^(?P<asset_id>[0-9]+)/$', views.assetDetail, name='assetDetail'), #
+    url(r'add/$', views.assetCreate.as_view(), name = 'add_asset'),
 ]
--- AssetData/views.py
+++ AssetData/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from .models import Asset
+from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django import forms
+
 
 def assetHome(request):
     return HttpResponse("<h1>Asset Data</h1>") 
 	
 def assetDetail(request, asset_id):
 	return HttpResponse("<h2>Details for asset ID:" + str(asset_id) + "</h2>")
+
+class assetCreate(CreateView):
+	model = Asset 
+	fields = ['cost', 'name', 'availability', 'outLength', 'currOwn']
+	
